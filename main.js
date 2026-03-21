8f884b9292aeb25a09d026e6a54be70f045d38cf
CloodDev <112552341+CloodDev@users.noreply.github.com>
2026-03-21 13:19:53 +0100

    chore: publish v1.3.8

--- main.js
+++ main.js
@@ -696,7 +696,7 @@ function createWindow() {
     }
   })
   if (isDev) mainWindow.loadURL('http://localhost:5173')
-  else mainWindow.loadFile(path.join(__dirname, 'dist', 'app.html'))
+  else mainWindow.loadFile(path.join(__dirname, 'dist', 'index.html'))
   
   mainWindow.on('close', (event) => {
     if (!app.isQuitting) {
